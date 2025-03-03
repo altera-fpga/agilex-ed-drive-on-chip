79063b6f098bc2bae0a07ff31360006ec616dbca
mevalver <esteban.valverde.vega@intel.com>
2025-03-03 02:31:11 -0800

    Adding Drive-On-Chip variants

---
+++ custom_user_subsystems/safety_subsystems/non_qpds_ip/doc_hardware/rtl/doc_hardware.sv
@@ -0,0 +1,142 @@
+/* ##################################################################################
+ * Copyright (C) 2025 Intel Corporation
+ *
+ * This software and the related documents are Intel copyrighted materials, and
+ * your use of them is governed by the express license under which they were
+ * provided to you ("License"). Unless the License provides otherwise, you may
+ * not use, modify, copy, publish, distribute, disclose or transmit this software
+ * or the related documents without Intel's prior written permission.
+ *
+ * This software and the related documents are provided as is, with no express
+ * or implied warranties, other than those that are expressly stated in the License.
+* ##################################################################################
+*/
+
+`default_nettype none
+
+import doc_hw_pkg_hw::*;
+
+module doc_hardware
+# (
+    parameter P_TEMP_WIDTH = 8
+) (
+    input wire hw_clk,
+    input wire hw_reset,
+
+    //Voltage Validation
+    (* preserve *) output logic pwr_good_p,
+    (* preserve *) output logic pwr_good_n,
+
+    //Temp Validation
+    (* preserve *) output logic temp_good_p,
+    (* preserve *) output logic temp_good_n,
+
+    // Explore CRAM + Mailbox Functionality...
+    (* preserve *) output logic cram_good_p,
+    (* preserve *) output logic cram_good_n
+);
+
+// +--------------------------------------------------
+// | Initial Signals
+// +--------------------------------------------------
+
+// Mailbox/MainCtrl Variables
+//  command interface
+logic            command_ready;
+logic            command_valid;
+logic [31:0]     command_data;
+logic            command_startofpacket;
+logic            command_endofpacket;
+
+//  response interface
+logic            response_ready;
+logic            response_valid;
+logic [31:0]     response_data;
+logic            response_startofpacket;
+logic            response_endofpacket;
+
+//Grabbed/Output Variables
+logic                                   voltage_good;
+logic                                   temperature_good;
+logic [P_NO_CHANNELS - 1 : 0][31:0]     voltage_collection;
+logic [P_NO_TEMP_CHANNELS - 1 : 0][7:0] temperature_collection;
+
+// +--------------------------------------------------
+// | Output Control
+// +--------------------------------------------------
+
+always_ff @(posedge hw_clk) begin
+    pwr_good_p <= voltage_good;
+    pwr_good_n <= ~voltage_good;
+
+    temp_good_p <= temperature_good;
+    temp_good_n <= ~temperature_good;
+
+    cram_good_p <= 1'b1;
+    cram_good_n <= 1'b0;
+end
+
+// +--------------------------------------------------
+// | Module Instances
+// +--------------------------------------------------
+
+// Main Control Inst
+// CMD & RSP driven in block below
+main_ctrl main(
+    .clk                        (hw_clk),
+    .reset                      (hw_reset),
+
+     //  command interface
+    .command_ready_i            (command_ready),
+    .command_valid_o            (command_valid),
+    .command_data_o             (command_data),
+    .command_startofpacket_o    (command_startofpacket),
+    .command_endofpacket_o      (command_endofpacket),
+
+    //  response interface
+    .response_ready_o           (response_ready),
+    .response_valid_i           (response_valid),
+    .response_data_i            (response_data),
+    .response_startofpacket_i   (response_startofpacket),
+    .response_endofpacket_i     (response_endofpacket),
+
+    .voltage_good               (voltage_good),
+    .temperature_good           (temperature_good),
+    .voltage_collection         (voltage_collection),
+    .temperature_collection     (temperature_collection)
+);
+
+ altera_config_stream_endpoint #(
+    .READY_LATENCY          (0),
+    .HAS_URGENT             (0),
+    .HAS_STATUS             (0),
+    .HAS_STREAM             (0),
+    .MAX_SIZE               (256),
+    .STREAM_WIDTH           (32),
+    .CLOCK_RATE_CLK         (0)
+) inst (
+    .clk                    (hw_clk),
+    .reset                  (hw_reset),
+    .command_ready          (command_ready),
+    .command_valid          (command_valid),
+    .command_data           (command_data),
+    .command_startofpacket  (command_startofpacket),
+    .command_endofpacket    (command_endofpacket),
+    .command_invalid        (),
+    .response_ready         (response_ready),
+    .response_valid         (response_valid),
+    .response_data          (response_data),
+    .response_startofpacket (response_startofpacket),
+    .response_endofpacket   (response_endofpacket),
+    .urgent_ready           (),
+    .urgent_valid           (1'b0),
+    .urgent_data            (32'd0),
+    .stream_ready           (),
+    .stream_valid           (1'b0),
+    .stream_data            (32'd0),
+    .stream_active          ()
+);
+
+endmodule
+
+`default_nettype wire
---
+++ custom_user_subsystems/safety_subsystems/non_qpds_ip/esl_safe_doc/rtl/esl_clk_check_slave_if.sv
@@ -0,0 +1,60 @@
+/* ##################################################################################
+ * Copyright (C) 2025 Intel Corporation
+ *
+ * This software and the related documents are Intel copyrighted materials, and
+ * your use of them is governed by the express license under which they were
+ * provided to you ("License"). Unless the License provides otherwise, you may
+ * not use, modify, copy, publish, distribute, disclose or transmit this software
+ * or the related documents without Intel's prior written permission.
+ *
+ * This software and the related documents are provided as is, with no express
+ * or implied warranties, other than those that are expressly stated in the License.
+* ##################################################################################
+*/
+
+// Description:
+// Avalon slave interface for CSR
+// ##########################################################################
+
+`default_nettype none
+
+module esl_clk_check_slave_if
+  (
+   // signals to connect to an Avalon-MM slave interface
+   input  wire [1:0]   csr_addr,            // only 3 registers so only 2 bits needed
+   output wire [31:0]  csr_readdata,        // internal reg values presented on this interface
+   // signals to connect to custom user logic
+   input  wire [23:0]  core_status,         // various core status bits are input on this bus
+   input  wire [23:0]  cut_count_store,     // stored value of the clock under test counter
+   input  wire [23:0]  ref_clk_tc_reg       // stored value if the refclk counter (should = TC)
+   );
+
+   // Simple decoder for the slave address
+
+   wire [2:0] address_decode;
+
+   assign     address_decode[0] = (csr_addr[1:0] == 2'b00); //status
+   assign     address_decode[1] = (csr_addr[1:0] == 2'b01); //cut_count
+   assign     address_decode[2] = (csr_addr[1:0] == 2'b10); //ref_clk_tc
+
+   // Decoded address is used to select which internal register is presented on
+   // read data port
+   // note this is a combinatorial always block so "reg" style definition required
+
+   reg [23:0] csr_readdata_reg;
+   always @ (*)
+     begin
+        case (address_decode[2:0])
+          3'b001: csr_readdata_reg  = core_status;
+          3'b010: csr_readdata_reg  = cut_count_store;
+          3'b100: csr_readdata_reg  = ref_clk_tc_reg;
+          default: csr_readdata_reg = core_status;
+        endcase // case(address_decode[2:0])
+     end
+
+   // wiring internal signal onto top level port
+   assign csr_readdata = {8'b0,csr_readdata_reg};
+
+endmodule
+
+`default_nettype wire
